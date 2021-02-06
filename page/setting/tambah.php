f915f6af276f43a22c75ff46d5f0e51ac04a825b
ihwan-trc <ihwan.2705@gmail.com>
2021-02-06 19:03:14 +0700

    modif page/setting/tambah.php dan ganti_pass.php

--- page/setting/tambah.php
+++ page/setting/tambah.php
@@ -5,13 +5,14 @@
         $desa = $_POST['desa'];
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $hash = password_hash($password, PASSWORD_DEFAULT);
         $level = $_POST['level'];
 
         // include database connection file
         include_once("../../asset/inc/config.php");
 
         // Insert barang data into table
-        $result = mysqli_query($koneksi, "INSERT INTO tb_user(username,password,level,nama_user) VALUES('$username','$password','$level','$desa')");
+        $result = mysqli_query($koneksi, "INSERT INTO tb_user(username,password,level,nama_user) VALUES('$username','$hash','$level','$desa')");
 
         // Show message when user added
         header("Location:../../index.php?page=user");
