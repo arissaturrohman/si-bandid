6bb5b8dd8c35d7a20bde0db7b9c0aa0f77d80163
ihwan-trc <ihwan.2705@gmail.com>
2021-02-18 16:51:29 +0700

    update pelaporan LPPD

--- page/pelaporan/lppd/lppd.php
+++ page/pelaporan/lppd/lppd.php
@@ -8,44 +8,107 @@
       <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <thead class="bg-dark text-white">
           <tr>
-            <th width="3%" class="text-center align-middle">No</th>
-            <th width="5%" class="text-center  align-middle">Desa</th>
-            <th class="text-center  align-middle">LPPD</th>
-            <th width="5%" class="text-center  align-middle">Validasi</th>
-            <th class="text-center  align-middle">Catatan</th>
+            <th width="3%" class="text-center">No</th>
+            <th width="5%" class="text-center">Desa</th>
+            <th class="text-center">LPPD</th>
+            <th width="5%" class="text-center">Validasi</th>
+            <th class="text-center">Catatan</th>
             <!-- Kolom action tampil jika user sudah login -->
-            <th width="18%" class="text-center  align-middle">Action</th>
+            <th width="18%" class="text-center">Action</th>
           </tr>
         </thead>
-        <tbody>         
+        <tbody>
+
+          <?php  
+          include "asset/inc/config.php";
+          $tahun = $_SESSION['tahun'];
+          if ($_SESSION['level']=="admin") {
+             $query = "SELECT * FROM tb_lppd
+                      INNER JOIN tb_user ON tb_lppd.id_user = tb_user.id_user AND tahun='$tahun' ORDER BY id_lppd DESC";
+            $result = mysqli_query($koneksi, $query);
+          }
+          if ($_SESSION['level']=="user") {
+            $id_user = $_SESSION['id_user'];
+            $query= "SELECT * FROM tb_lppd
+                      INNER JOIN tb_user ON tb_lppd.id_user = tb_user.id_user AND tb_lppd.id_user=$id_user AND tahun='$tahun' ORDER BY id_lppd DESC";
+            $result = mysqli_query($koneksi, $query);
+          }  
+            //mengecek apakah ada error ketika menjalankan query
+            if(!$result){
+              die ("Query Error: ".mysqli_errno($koneksi).
+                 " - ".mysqli_error($koneksi));
+            }
+            //buat perulangan untuk element tabel dari data lppd
+            $no = 1;
+            while($data = mysqli_fetch_assoc($result))
+            {
+            ?>
           <tr>
-            <td class="text-center">1</td>
-            <td>Mranggen</td>
+            <td class="text-center"><?php echo "$no"; ?></td>
+            <td><?php echo $data['nama_user']; ?></td>
             <!-- Bisa di download untuk di koreksi, muncul link jika user sudah login jika belum login jangan tampilkan linknya-->
-            <td>file lppd</td>
+            <td>
+              <a href="page/pelaporan/lppd/file/lppd/<?php echo $data['lppd']; ?>">
+              <i class="fas fa-download" id="downloadlppd">&nbsp;&nbsp;</i></a><?php echo $data['lppd']; ?>
+            </td>
 
             <!-- Jika catatan "revisi" kolom berwarna merah kalau "diterima" warna hijau -->
-            
-            <td>Revisi</td>
-            <td>Anggaran salah</td>
+            <?php 
+              $validasi = $data['validasi'];
+              $jumlah_karakter    =strlen($validasi);
+              // revisi
+              if ($jumlah_karakter == 6) {
+              $color = "class='bg-danger text-white';";
+              }
+              // diterima
+              elseif ($jumlah_karakter == 8){
+              $color = "class='bg-success text-white';";
+              }
+              // menunggu validasi
+              elseif ($jumlah_karakter == 17){
+              $color = "class='bg-info text-white';";
+              }
+              // menunggu validasi revisi
+              elseif ($jumlah_karakter ){
+              $color = "class='bg-primary text-white';";
+              }
+              ?>
+            <td <?= $color ?> ><?php echo $validasi; ?></td>
+            <td><?php echo $data['catatan']; ?></td>
             <td>
               <!-- tombol validasi muncul jika login level admin -->
-              
-              <a href="#" class="btn btn-sm btn-success validasi" data-toggle="modal" data-target="#validasi_lppdModal"><i class="fas fa-edit"></i> validasi</a>
+              <?php 
+                if ($_SESSION['level']=="admin") {
+                 echo ''?> <a href="#" class="btn btn-sm btn-success validasi" data-toggle="modal" data-target="#lppdModal"
+                      data-idlppd="<?php echo $data["id_lppd"];?>"
+                      data-validasi="<?php echo $data["validasi"];?>"
+                      data-catatan="<?php echo $data["catatan"];?>"
+                      ><i class="fas fa-check"></i> validasi</a>
+                <?php  ;
+                }else{
+                 echo '';
+                }
+               ?>
 
-              <a href="#" class="btn btn-sm btn-info edit" data-toggle="modal" data-target="#edit_lppdModal"><i class="fas fa-edit"></i> edit</a>
+              <a href="#" class="btn btn-sm btn-info edit" data-toggle="modal" data-target="#edit_lppdModal"
+              data-id_lppd="<?php echo $data['id_lppd'];?>"
+              data-lppd="<?php echo $data['lppd'];?>"
+              ><i class="fas fa-edit"></i> edit</a>
 
-              <a href="#" onclick="return confirm('Yakin Hapus?')" class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i> delete</a>
+              <a href="page/pelaporan/lppd/hapus.php?id_lppd=<?php echo $data['id_lppd'];?>" onclick="return confirm('Yakin Hapus?')" class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i> delete</a>
             </td>
           </tr>
+          <?php
+          $no++;
+          }
+          ?>
         </tbody>
       </table>
     </div>
   </div>
 </div>
 
-
-<!-- Modal add -->
+<!-- Modal add lppd -->
 <div class="modal fade" id="add_lppdModal" tabindex="-1" aria-labelledby="lppdModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -56,15 +119,20 @@
         </button>
       </div>
       <div class="modal-body">
-        <form method="POST" action="" enctype="multipart/form-data" >
+        <form method="POST" action="page/pelaporan/lppd/tambah.php" enctype="multipart/form-data" >
+          <input type="hidden" name="valid" id="valid" value="Menunggu Validasi">
+          <input type="hidden" name="id_user" id="iduser" value="<?= $_SESSION['id_user']; ?>">
+          <input type="hidden" name="tahun" id="tahun" value="<?= date("Y"); ?>">
           <div class="form-group">
-            <label for="">LPPD</label>
+            <label for="">File LPPD</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="" name="" required="" onchange="return validasiFile()"/>
+              <input type="file" class="custom-file-input" id="lppd" name="lppd" required="" onchange="return validasiFile()"/>
               <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
-            <span class="text-danger"><small>* File ekstensi Excel, Word & pdf </small></span>
           </div>
+          <span class="text-danger"><small>* File yang diperbolehkan Excel, Word & pdf </small></span><br>
+          <span class="text-danger"><small>* Ukuran file maksimal 5mb</small></span>
+        
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
@@ -75,7 +143,25 @@
   </div>
 </div>
 
-<!-- Modal edit -->
+<script type="text/javascript">
+var uploadField = document.getElementById("lppd");
+uploadField.onchange = function() {
+    if(this.files[0].size > 5000000){ // ini untuk ukuran 800KB, 1000000 untuk 1 MB.
+       alert("Maaf. File Terlalu Besar ! Maksimal Upload 5 MB");
+       this.value = "";
+    };
+        var inputFile = document.getElementById('lppd');
+        var pathFile = inputFile.value;
+        var ekstensiOk = /(\.pdf|\.xlsx|\.xls|\.doc|\.docx)$/i;
+        if(!ekstensiOk.exec(pathFile)){
+            alert('Silahkan upload file yang memiliki ekstensi .pdf.xlxs.docx');
+            inputFile.value = '';
+            return false;
+        }
+    };
+</script>
+
+<!-- Modal edit lppd -->
 <div class="modal fade" id="edit_lppdModal" tabindex="-1" aria-labelledby="lppdModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -84,17 +170,21 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div> 
       <div class="modal-body">
-        <form action="" method="POST" enctype="multipart/form-data">
+        <form action="page/pelaporan/lppd/edit.php" method="POST" enctype="multipart/form-data">
+          <input type="hidden" name="id_lppd" id="id_lppd">
+          <input type="hidden" name="validasi" value="Menunggu Revisi Divalidasi">
+          <input type="hidden" name="catatan">
           <div class="form-group">
-            <label for="">LPPD</label>
+          <label for="lppd">File LPPD</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" name="" id=""  onchange="return validasiFile()"/>
+              <input type="file" class="custom-file-input" name="lppd" id="lppdid" onchange="return validasiFile()"/>
               <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
-            <span class="text-danger"><small>* File ekstensi Excel, Word & pdf </small></span>
           </div>
+          <span class="text-danger"><small>* File yang diperbolehkan Excel, Word & pdf </small></span><br>
+          <span class="text-danger"><small>* Ukuran file maksimal 5mb</small></span>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
@@ -105,9 +195,46 @@
   </div>
 </div>
 
+<script src="asset/vendor/jquery/jquery.min.js"></script>
+<script type="text/javascript">
+  $('.edit').click(function(){
+    $('#edit_lppdModal').modal();
+    var id_lppd = $(this).attr('data-id_lppd')
+    var lppd = $(this).attr('data-lppd')
+    var id_user = $(this).attr('data-iduser')
+    var tahun = $(this).attr('data-tahun')
+    var validasi = $(this).attr('data-validasi')
+    var catatan = $(this).attr('data-catatan')
+    $('#id_lppd').val(id_lppd)
+    $('#lppdid').val(lppd)
+    $('#perd').val(perdes)
+    $('#iduser').val(id_user)
+    $('#tahun').val(tahun)
+    $('#validasi').val(validasi)
+    $('#catatan').val(catatan)
+  })
+</script>
 
-<!-- Modal validasi -->
-<div class="modal fade" id="validasi_lppdModal" tabindex="-1" aria-labelledby="lppdModalLabel" aria-hidden="true">
+<script type="text/javascript">
+var uploadField = document.getElementById("lppdid");
+uploadField.onchange = function() {
+    if(this.files[0].size > 5000000){ // ini untuk ukuran 800KB, 1000000 untuk 1 MB.
+       alert("Maaf. File Terlalu Besar ! Maksimal Upload 5 MB");
+       this.value = "";
+    };
+        var inputFile = document.getElementById('lppdid');
+        var pathFile = inputFile.value;
+        var ekstensiOk = /(\.pdf|\.xlsx|\.xls|\.doc|\.docx)$/i;
+        if(!ekstensiOk.exec(pathFile)){
+            alert('Silahkan upload file yang memiliki ekstensi .pdf.xlxs.docx');
+            inputFile.value = '';
+            return false;
+        }
+    };
+</script>
+
+<!-- Modal validasi lppd -->
+<div class="modal fade" id="lppdModal" tabindex="-1" aria-labelledby="lppdModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -115,20 +242,28 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div>  
       <div class="modal-body">
-        <form method="POST" action="">
+        <form method="POST" action="page/pelaporan/lppd/validasi.php">
+          <input type="hidden" name="id_lppd" id="idlppd">
           <div class="form-group">
             <label for="validasi">Validasi</label>
-            <select class="form-control" name="debug" id="valid" tabindex="-1">
+            <select class="form-control" name="debug" id="validasi" tabindex="-1">
               <option>-- Pilih --</option>
               <option value="Revisi">Revisi</option>
               <option value="Diterima">Diterima</option>
             </select>
+              <script>
+                $(function() {
+                  $("#validasi").on("change", function() {
+                    $("#debug").text($("#validasi").val());
+                  }).trigger("change");
+                });
+              </script>
           </div>
           <div class="form-group">
             <label for="catatan">Catatan</label>
-            <textarea class="form-control" name="catat" id="catat" rows="3"></textarea>
+            <textarea class="form-control" name="catatan" id="catatan" rows="3"></textarea>
           </div>
       </div>
       <div class="modal-footer">
@@ -139,3 +274,16 @@
     </div>
   </div>
 </div>
+
+<script src="asset/vendor/jquery/jquery.min.js"></script>
+<script type="text/javascript">
+  $('.validasi').click(function(){
+    $('#lppdModal').modal();
+    var id_lppd = $(this).attr('data-idlppd')
+    var validasi = $(this).attr('data-validasi')
+    var catatan = $(this).attr('data-catatan')
+    $('#idlppd').val(id_lppd)
+    $('#validasi').val(validasi)
+    $('#catatan').val(catatan)
+  })
+</script>
