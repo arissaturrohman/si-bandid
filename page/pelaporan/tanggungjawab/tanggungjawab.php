d485865fc8c339c17a737e0e4e2ae530fa7327dd
ihwan-trc <ihwan.2705@gmail.com>
2021-02-18 17:42:51 +0700

    update pelaporan Pertanggungjawaban

--- page/pelaporan/tanggungjawab/tanggungjawab.php
+++ page/pelaporan/tanggungjawab/tanggungjawab.php
@@ -1,79 +1,148 @@
 <h1 class="h3 mb-4 text-gray-800">Halaman Pertanggungjawaban</h1>
 <div class="card shadow mb-4">
   <div class="card-header py-3">
-    <a href="#" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#add_tanggungjawabModal">Tambah</a>
+    <a href="#" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#add_tanggung_jawabModal">Tambah</a>
   </div>
   <div class="card-body">
     <div class="table-responsive">
       <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <thead class="bg-dark text-white">
           <tr>
-            <th width="3%" class="text-center align-middle">No</th>
-            <th width="5%" class="text-center  align-middle">Desa</th>
-            <th class="text-center  align-middle">Pertanggungjawaban</th>
-            <th class="text-center  align-middle">Perdes</th>
-            <th width="5%" class="text-center  align-middle">Validasi</th>
-            <th class="text-center  align-middle">Catatan</th>
+            <th width="3%" class="text-center">No</th>
+            <th width="5%" class="text-center">Desa</th>
+            <th class="text-center">Pertanggungjawaban</th>
+            <th class="text-center">Perdes</th>
+            <th width="5%" class="text-center">Validasi</th>
+            <th class="text-center">Catatan</th>
             <!-- Kolom action tampil jika user sudah login -->
-            <th width="18%" class="text-center  align-middle">Action</th>
+            <th width="18%" class="text-center">Action</th>
           </tr>
         </thead>
-        <tbody>         
+        <tbody>
+
+          <?php  
+          include "asset/inc/config.php";
+          $tahun = $_SESSION['tahun'];
+          if ($_SESSION['level']=="admin") {
+             $query = "SELECT * FROM tb_tanggung_jawab
+                      INNER JOIN tb_user ON tb_tanggung_jawab.id_user = tb_user.id_user AND tahun='$tahun' ORDER BY id_tanggung_jawab DESC";
+            $result = mysqli_query($koneksi, $query);
+          }
+          if ($_SESSION['level']=="user") {
+            $id_user = $_SESSION['id_user'];
+            $query= "SELECT * FROM tb_tanggung_jawab
+                      INNER JOIN tb_user ON tb_tanggung_jawab.id_user = tb_user.id_user AND tb_tanggung_jawab.id_user=$id_user AND tahun='$tahun' ORDER BY id_tanggung_jawab DESC";
+            $result = mysqli_query($koneksi, $query);
+          }  
+            //mengecek apakah ada error ketika menjalankan query
+            if(!$result){
+              die ("Query Error: ".mysqli_errno($koneksi).
+                 " - ".mysqli_error($koneksi));
+            }
+            //buat perulangan untuk element tabel dari data tanggung_jawab
+            $no = 1;
+            while($data = mysqli_fetch_assoc($result))
+            {
+            ?>
           <tr>
-            <td class="text-center">1</td>
-            <td>Mranggen</td>
+            <td class="text-center"><?php echo "$no"; ?></td>
+            <td><?php echo $data['nama_user']; ?></td>
             <!-- Bisa di download untuk di koreksi, muncul link jika user sudah login jika belum login jangan tampilkan linknya-->
-            <td>file Pertanggungjawaban</td>
-            <td>file Perdes</td>
+            <td>
+              <a href="page/pelaporan/tanggungjawab/file/tanggungjawab/<?php echo $data['tanggung_jawab']; ?>">
+              <i class="fas fa-download" id="downloadtanggungjawab">&nbsp;&nbsp;</i></a><?php echo $data['tanggung_jawab']; ?>
+            </td>
+            <td><a href="page/pelaporan/tanggungjawab/file/perdes/<?php echo $data['perdes']; ?>"><i class="fas fa-download" id="downloadperdes">&nbsp;&nbsp;</i></a><?php echo $data['perdes']; ?></td>
 
             <!-- Jika catatan "revisi" kolom berwarna merah kalau "diterima" warna hijau -->
-            
-            <td>Revisi</td>
-            <td>Anggaran salah</td>
+            <?php 
+              $validasi = $data['validasi'];
+              $jumlah_karakter    =strlen($validasi);
+              // revisi
+              if ($jumlah_karakter == 6) {
+              $color = "class='bg-danger text-white';";
+              }
+              // diterima
+              elseif ($jumlah_karakter == 8){
+              $color = "class='bg-success text-white';";
+              }
+              // menunggu validasi
+              elseif ($jumlah_karakter == 17){
+              $color = "class='bg-info text-white';";
+              }
+              // menunggu validasi revisi
+              elseif ($jumlah_karakter ){
+              $color = "class='bg-primary text-white';";
+              }
+              ?>
+            <td <?= $color ?> ><?php echo $validasi; ?></td>
+            <td><?php echo $data['catatan']; ?></td>
             <td>
               <!-- tombol validasi muncul jika login level admin -->
-              
-              <a href="#" class="btn btn-sm btn-success validasi" data-toggle="modal" data-target="#validasi_tanggungjawabModal"><i class="fas fa-edit"></i> validasi</a>
+              <?php 
+                if ($_SESSION['level']=="admin") {
+                 echo ''?> <a href="#" class="btn btn-sm btn-success validasi" data-toggle="modal" data-target="#tanggung_jawabModal"
+                      data-idtanggung_jawab="<?php echo $data["id_tanggung_jawab"];?>"
+                      data-validasi="<?php echo $data["validasi"];?>"
+                      data-catatan="<?php echo $data["catatan"];?>"
+                      ><i class="fas fa-check"></i> validasi</a>
+                <?php  ;
+                }else{
+                 echo '';
+                }
+               ?>
 
-              <a href="#" class="btn btn-sm btn-info edit" data-toggle="modal" data-target="#edit_tanggungjawabModal"><i class="fas fa-edit"></i> edit</a>
+              <a href="#" class="btn btn-sm btn-info edit" data-toggle="modal" data-target="#edit_tanggung_jawabModal"
+              data-id_tanggung_jawab="<?php echo $data['id_tanggung_jawab'];?>"
+              data-tanggung_jawab="<?php echo $data['tanggung_jawab'];?>"
+              data-perdes="<?php echo $data['perdes'];?>"
+              ><i class="fas fa-edit"></i> edit</a>
 
-              <a href="#" onclick="return confirm('Yakin Hapus?')" class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i> delete</a>
+              <a href="page/pelaporan/tanggungjawab/hapus.php?id_tanggung_jawab=<?php echo $data['id_tanggung_jawab'];?>" onclick="return confirm('Yakin Hapus?')" class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i> delete</a>
             </td>
           </tr>
+          <?php
+          $no++;
+          }
+          ?>
         </tbody>
       </table>
     </div>
   </div>
 </div>
 
-
-<!-- Modal add -->
-<div class="modal fade" id="add_tanggungjawabModal" tabindex="-1" aria-labelledby="tanggungjawabModalLabel" aria-hidden="true">
+<!-- Modal add tanggung_jawab -->
+<div class="modal fade" id="add_tanggung_jawabModal" tabindex="-1" aria-labelledby="tanggung_jawabModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="tanggungjawabModalLabel">Form Add Pertanggungjawaban</h5>
+        <h5 class="modal-title" id="tanggung_jawabModalLabel">Form Add Pertanggungjawaban</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <form method="POST" action="" enctype="multipart/form-data" >
+        <form method="POST" action="page/pelaporan/tanggungjawab/tambah.php" enctype="multipart/form-data" >
+          <input type="hidden" name="valid" id="valid" value="Menunggu Validasi">
+          <input type="hidden" name="id_user" id="iduser" value="<?= $_SESSION['id_user']; ?>">
+          <input type="hidden" name="tahun" id="tahun" value="<?= date("Y"); ?>">
           <div class="form-group">
-            <label for="">Pertanggungjawaban</label>
+            <label for="">File Pertanggungjawaban</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="" name="" required="" onchange="return validasiFile()"/>
+              <input type="file" class="custom-file-input" id="tanggung_jawab" name="tanggung_jawab" required="" onchange="return validasiFile()"/>
               <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
           </div>
           <div class="form-group">
-            <label for="">Perdes</label>
+            <label for="">Perdes Pertanggungjawaban</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="" name="" required="" onchange="return validasiFile()"/>
+              <input type="file" class="custom-file-input" id="perdes" name="perdes" required="" onchange="return validasiFile()"/>
               <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
-            <span class="text-danger"><small>* File ekstensi Excel, Word & pdf </small></span>
           </div>
+          <span class="text-danger"><small>* File yang diperbolehkan Excel, Word & pdf </small></span><br>
+          <span class="text-danger"><small>* Ukuran file maksimal 5mb</small></span>
+        
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
@@ -84,33 +153,71 @@
   </div>
 </div>
 
-<!-- Modal edit -->
-<div class="modal fade" id="edit_tanggungjawabModal" tabindex="-1" aria-labelledby="tanggungjawabModalLabel" aria-hidden="true">
+<script type="text/javascript">
+var uploadField = document.getElementById("tanggung_jawab");
+uploadField.onchange = function() {
+    if(this.files[0].size > 5000000){ // ini untuk ukuran 800KB, 1000000 untuk 1 MB.
+       alert("Maaf. File Terlalu Besar ! Maksimal Upload 5 MB");
+       this.value = "";
+    };
+        var inputFile = document.getElementById('tanggung_jawab');
+        var pathFile = inputFile.value;
+        var ekstensiOk = /(\.pdf|\.xlsx|\.xls|\.doc|\.docx)$/i;
+        if(!ekstensiOk.exec(pathFile)){
+            alert('Silahkan upload file yang memiliki ekstensi .pdf.xlxs.docx');
+            inputFile.value = '';
+            return false;
+        }
+    };
+
+  var uploadField = document.getElementById("perdes");
+  uploadField.onchange = function() {
+    if(this.files[0].size > 5000000){ // ini untuk ukuran 800KB, 1000000 untuk 1 MB.
+       alert("Maaf. File Terlalu Besar ! Maksimal Upload 5 MB");
+       this.value = "";
+    };
+        var inputFile = document.getElementById('perdes');
+        var pathFile = inputFile.value;
+        var ekstensiOk = /(\.pdf|\.xlsx|\.xls|\.doc|\.docx)$/i;
+        if(!ekstensiOk.exec(pathFile)){
+            alert('Silahkan upload file yang memiliki ekstensi .pdf.xlxs.docx');
+            inputFile.value = '';
+            return false;
+        }
+    };
+</script>
+
+<!-- Modal edit tanggung_jawab -->
+<div class="modal fade" id="edit_tanggung_jawabModal" tabindex="-1" aria-labelledby="tanggung_jawabModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="tanggungjawabModalLabel">Form Edit tanggungjawab</h5>
+        <h5 class="modal-title" id="tanggung_jawabModalLabel">Form Edit Pertanggungjawaban</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div> 
       <div class="modal-body">
-        <form action="" method="POST" enctype="multipart/form-data">
+        <form action="page/pelaporan/tanggungjawab/edit.php" method="POST" enctype="multipart/form-data">
+          <input type="hidden" name="id_tanggung_jawab" id="id_tanggung_jawab">
+          <input type="hidden" name="validasi" value="Menunggu Revisi Divalidasi">
+          <input type="hidden" name="catatan">
           <div class="form-group">
-            <label for="">Pertanggungjawaban</label>
+          <label for="tanggung_jawab">File Pertanggungjawaban</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" name="" id=""  onchange="return validasiFile()"/>
+              <input type="file" class="custom-file-input" name="tanggung_jawab" id="tanggung" onchange="return validasiFile()"/>
               <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
           </div>
           <div class="form-group">
-            <label for="">Perdes</label>
+          <label for="perdes">Perdes Pertanggungjawaban</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" name="" id=""  onchange="return validasiFile()"/>
+              <input type="file" class="custom-file-input" name="perdes" id="perd" onchange="return validasiFile()"/>
               <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
-            <span class="text-danger"><small>* File ekstensi Excel, Word & pdf </small></span>
           </div>
+          <span class="text-danger"><small>* File yang diperbolehkan Excel, Word & pdf </small></span><br>
+          <span class="text-danger"><small>* Ukuran file maksimal 5mb</small></span>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
@@ -121,30 +228,92 @@
   </div>
 </div>
 
+<script src="asset/vendor/jquery/jquery.min.js"></script>
+<script type="text/javascript">
+  $('.edit').click(function(){
+    $('#edit_tanggung_jawabModal').modal();
+    var id_tanggung_jawab = $(this).attr('data-id_tanggung_jawab')
+    var tanggung_jawab = $(this).attr('data-tanggung_jawab')
+    var perdes = $(this).attr('data-perdes')
+    var id_user = $(this).attr('data-iduser')
+    var tahun = $(this).attr('data-tahun')
+    var validasi = $(this).attr('data-validasi')
+    var catatan = $(this).attr('data-catatan')
+    $('#id_tanggung_jawab').val(id_tanggung_jawab)
+    $('#tanggung').val(tanggung_jawab)
+    $('#perd').val(perdes)
+    $('#iduser').val(id_user)
+    $('#tahun').val(tahun)
+    $('#validasi').val(validasi)
+    $('#catatan').val(catatan)
+  })
+</script>
 
-<!-- Modal validasi -->
-<div class="modal fade" id="validasi_tanggungjawabModal" tabindex="-1" aria-labelledby="tanggungjawabModalLabel" aria-hidden="true">
+<script type="text/javascript">
+var uploadField = document.getElementById("tanggung");
+uploadField.onchange = function() {
+    if(this.files[0].size > 5000000){ // ini untuk ukuran 800KB, 1000000 untuk 1 MB.
+       alert("Maaf. File Terlalu Besar ! Maksimal Upload 5 MB");
+       this.value = "";
+    };
+        var inputFile = document.getElementById('tanggung');
+        var pathFile = inputFile.value;
+        var ekstensiOk = /(\.pdf|\.xlsx|\.xls|\.doc|\.docx)$/i;
+        if(!ekstensiOk.exec(pathFile)){
+            alert('Silahkan upload file yang memiliki ekstensi .pdf.xlxs.docx');
+            inputFile.value = '';
+            return false;
+        }
+    };
+
+  var uploadField = document.getElementById("perd");
+  uploadField.onchange = function() {
+    if(this.files[0].size > 5000000){ // ini untuk ukuran 800KB, 1000000 untuk 1 MB.
+       alert("Maaf. File Terlalu Besar ! Maksimal Upload 5 MB");
+       this.value = "";
+    };
+        var inputFile = document.getElementById('perd');
+        var pathFile = inputFile.value;
+        var ekstensiOk = /(\.pdf|\.xlsx|\.xls|\.doc|\.docx)$/i;
+        if(!ekstensiOk.exec(pathFile)){
+            alert('Silahkan upload file yang memiliki ekstensi .pdf.xlxs.docx');
+            inputFile.value = '';
+            return false;
+        }
+    };
+</script>
+
+<!-- Modal validasi tanggung_jawab -->
+<div class="modal fade" id="tanggung_jawabModal" tabindex="-1" aria-labelledby="tanggung_jawabModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="tanggungjawabModalLabel">Form Validasi tanggungjawab</h5>
+        <h5 class="modal-title" id="tanggung_jawabModalLabel">Form Validasi Pertanggungjawaban</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div>  
       <div class="modal-body">
-        <form method="POST" action="">
+        <form method="POST" action="page/pelaporan/tanggungjawab/validasi.php">
+          <input type="hidden" name="id_tanggung_jawab" id="idtanggung_jawab">
           <div class="form-group">
             <label for="validasi">Validasi</label>
-            <select class="form-control" name="debug" id="valid" tabindex="-1">
+            <select class="form-control" name="debug" id="validasi" tabindex="-1">
               <option>-- Pilih --</option>
               <option value="Revisi">Revisi</option>
               <option value="Diterima">Diterima</option>
             </select>
+              <script>
+                $(function() {
+                  $("#validasi").on("change", function() {
+                    $("#debug").text($("#validasi").val());
+                  }).trigger("change");
+                });
+              </script>
           </div>
           <div class="form-group">
             <label for="catatan">Catatan</label>
-            <textarea class="form-control" name="catat" id="catat" rows="3"></textarea>
+            <textarea class="form-control" name="catatan" id="catatan" rows="3"></textarea>
           </div>
       </div>
       <div class="modal-footer">
@@ -155,3 +324,16 @@
     </div>
   </div>
 </div>
+
+<script src="asset/vendor/jquery/jquery.min.js"></script>
+<script type="text/javascript">
+  $('.validasi').click(function(){
+    $('#tanggung_jawabModal').modal();
+    var id_tanggung_jawab = $(this).attr('data-idtanggung_jawab')
+    var validasi = $(this).attr('data-validasi')
+    var catatan = $(this).attr('data-catatan')
+    $('#idtanggung_jawab').val(id_tanggung_jawab)
+    $('#validasi').val(validasi)
+    $('#catatan').val(catatan)
+  })
+</script>
